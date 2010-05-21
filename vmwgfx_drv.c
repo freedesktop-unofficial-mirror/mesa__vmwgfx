2256276821a8499d92aadacb2f9ce9a039e4b790
Thomas Hellstrom <thellstrom@vmware.com>
2010-05-21 15:16:47 +0200

    vmwgfx: Remove some leftover debug messages.
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>

--- vmwgfx_drv.c
+++ vmwgfx_drv.c
@@ -507,8 +507,6 @@ static int vmw_driver_unload(struct drm_device *dev)
 {
 	struct vmw_private *dev_priv = vmw_priv(dev);
 
-	DRM_INFO(VMWGFX_DRIVER_NAME " unload.\n");
-
 	unregister_pm_notifier(&dev_priv->pm_nb);
 
 	if (!dev_priv->handover) {
@@ -685,7 +683,6 @@ static int vmw_master_create(struct drm_device *dev,
 {
 	struct vmw_master *vmaster;
 
-	DRM_INFO("Master create.\n");
 	vmaster = kzalloc(sizeof(*vmaster), GFP_KERNEL);
 	if (unlikely(vmaster == NULL))
 		return -ENOMEM;
@@ -702,7 +699,6 @@ static void vmw_master_destroy(struct drm_device *dev,
 {
 	struct vmw_master *vmaster = vmw_master(master);
 
-	DRM_INFO("Master destroy.\n");
 	master->driver_priv = NULL;
 	kfree(vmaster);
 }
@@ -717,8 +713,6 @@ static int vmw_master_set(struct drm_device *dev,
 	struct vmw_master *active = dev_priv->active_master;
 	struct vmw_master *vmaster = vmw_master(file_priv->master);
 	int ret = 0;
-
-	DRM_INFO("Master set.\n");
 
 	if (!dev_priv->handover && dev_priv->stealth) {
 		vmw_kms_save_vga(dev_priv);
@@ -771,8 +765,6 @@ static void vmw_master_drop(struct drm_device *dev,
 	struct vmw_fpriv *vmw_fp = vmw_fpriv(file_priv);
 	struct vmw_master *vmaster = vmw_master(file_priv->master);
 	int ret;
-
-	DRM_INFO("Master drop.\n");
 
 	/**
 	 * Make sure the master doesn't disappear while we have
