55ccdd2ecaec3b462a6d114dc102d5085efe8900
Thomas Hellstrom <thellstrom@vmware.com>
2014-11-12 22:07:19 +0100

    ttm: ttm object security fixes for render nodes
    When a client looks up a ttm object, don't look it up through the device hash
    table, but rather from the file hash table. That makes sure that the client
    has indeed put a reference on the object, or in gem terms, has opened
    the object; either using prime or using the global "name".
    
    To avoid a performance loss, make sure the file hash table entries can be
    looked up from under an RCU lock, and as a consequence, replace the rwlock
    with a spinlock, since we never need to take it in read mode only anymore.
    
    Finally add a ttm object lookup function for the device hash table, that is
    intended to be used when we put a ref object on a base object or, in  gem terms,
    when we open the object.
    
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- vmwgfx_surface.c
+++ vmwgfx_surface.c
@@ -894,6 +894,7 @@ out_unlock:
 int vmw_surface_reference_ioctl(struct drm_device *dev, void *data,
 				struct drm_file *file_priv)
 {
+	struct vmw_private *dev_priv = vmw_priv(dev);
 	union drm_vmw_surface_reference_arg *arg =
 	    (union drm_vmw_surface_reference_arg *)data;
 	struct drm_vmw_surface_arg *req = &arg->req;
@@ -905,7 +906,7 @@ int vmw_surface_reference_ioctl(struct drm_device *dev, void *data,
 	struct ttm_base_object *base;
 	int ret = -EINVAL;
 
-	base = ttm_base_object_lookup(tfile, req->sid);
+	base = ttm_base_object_lookup_for_ref(dev_priv->tdev, req->sid);
 	if (unlikely(base == NULL)) {
 		DRM_ERROR("Could not find surface to reference.\n");
 		return -EINVAL;
@@ -1318,7 +1319,7 @@ int vmw_gb_surface_reference_ioctl(struct drm_device *dev, void *data,
 	uint32_t backup_handle;
 	int ret = -EINVAL;
 
-	base = ttm_base_object_lookup(tfile, req->sid);
+	base = ttm_base_object_lookup_for_ref(dev_priv->tdev, req->sid);
 	if (unlikely(base == NULL)) {
 		DRM_ERROR("Could not find surface to reference.\n");
 		return -EINVAL;
